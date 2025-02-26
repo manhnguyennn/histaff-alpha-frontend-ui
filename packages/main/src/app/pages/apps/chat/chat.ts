3ec2052e4bd445368753465c02676ba212d0a83f
manhnguyennn <60807101+manhnguyennn@users.noreply.github.com>
2025-02-26 17:28:46 +0700

    M

--- packages/main/src/app/pages/apps/chat/chat.ts
+++
@@ -1,7 +0,0 @@
-export interface Message {
-    id: string;
-    from: string;
-    subject: string;
-    photo: string;
-    chat: { type: string; msg: string; date: Date }[];
-  }
--- packages/main/src/app/pages/apps/taskboard/taskbord.ts
+++
@@ -1,9 +0,0 @@
-export interface Todos {
-  id: number;
-  title: string;
-  description: string;
-  class?: string;
-  date?: string;
-  taskProperty?: string;
-  imageUrl?: string;
-}
